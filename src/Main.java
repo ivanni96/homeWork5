5dfdca0df49f99755c7610baf3ac635b7b5af2fd
ivan <ivanni95@gmail.com>
2022-10-03 21:03:47 +0400

    Решил задачу 1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Задача 1
+
+        int clientOS = 1;
+
+        if (clientOS == 1){
+            System.out.println("Установите версию приложения для iOS по ссылке");}
+            if (clientOS == 0){
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        }
+
+    }
+
+
+
