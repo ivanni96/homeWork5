7adb098c238f43c83b1b29d3eb4e87aae6a87f47
ivan <ivanni95@gmail.com>
2022-10-04 19:26:01 +0400

    Решил задачу 2

--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,28 @@ public class Main {
             if (clientOS == 0){
                 System.out.println("Установите версию приложения для Android по ссылке");
             }
+
+        // Задача 2
+
+        int clientOS2 = 0;
+        int clientDeviceYear = 2015;
+
+        if (clientOS2 == 1 && clientDeviceYear >= 2015){
+            System.out.println("Установите версию приложения для iOS по ссылке");}
+
+        if (clientOS2 == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        if (clientOS2 == 0 && clientDeviceYear >= 2015){
+            System.out.println("Установите версию приложения для Android по ссылке");}
+        if (clientDeviceYear < 2015 && clientOS2 == 0) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                }
+            }
+
         }
 
-    }
+
 
 
 
